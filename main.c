bbc78b353b02889a80ecf568f0cc7abf1f835578
Kaliiixx <modepadu95@riseup.net>
2015-03-05 00:20:54 +0100

    Open a JSON file

--- main.c
+++ main.c
@@ -1,43 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "frozen.h"
+
+int openFile(char file[], char **json);
+
 
 int main(int argc, char *argv[])
 {
-	int fileLength = 0, i = 0;
-	FILE* jsonFile = NULL;
+	struct json_token *arr, *tok;
 	char *json = NULL;
-
-	jsonFile = fopen("livre.json", "r");
-
-	if ( jsonFile != NULL )
-	{
-		for (fileLength=0; fgetc(jsonFile) != EOF; i++);
-		if(
-		json = malloc(fileLength * sizeof(char));
-		
-		printf("%s\n",json);
 	
-		for(i=0; i<= fileLength; json[i]=fgetc(jsonFile) , i++)
-		{
-			if (json[i] == '\n' || json[i] == '\t')
-			{
-				json[i] = ' ';
-			}
-			printf("%c\n",json[i]);
-		}
-
+	if (openFile("livre.json", &json))
+	{
 
 		// Tokenize json string, fill in tokens array
+		arr = parse_json2(json, strlen(json));
 
 		//Search for parameter "bar" and print it's value
-		// Do not forget to free allocated tokens array
-		
-	free(json);
-	fclose(jsonFile);
-	}
-	else { printf("Error : Could not open the file"); }
+		tok = find_json_token(arr, "key_1");
+		printf("Value of bar is: [%.*s]\n", tok->len, tok->ptr);
 
+		//Do not forget to free allocated tokens array
+		free(arr);	
+	}
+	
 	return 0;
 }
+
+int openFile(char file[], char **json)
+{
+	int fileLength = 0, i = 0, c=0;
+	FILE* jsonFile = NULL;
+
+	jsonFile = fopen(file, "r");
+	if ( jsonFile != NULL )
+	{		
+		for (fileLength=0; fgetc(jsonFile) != EOF; fileLength++);
+		*json = malloc(fileLength * sizeof(char));
+		
+		rewind(jsonFile);
+		
+		
+		for(i=0; i< fileLength; i++)
+		{
+		
+			c = fgetc(jsonFile);
+		
+			if( isalnum(c) || ispunct(c))
+	
+			{
+		
+				(*json)[i] = (char)c;
+		
+			}
+		
+			else
+		
+			{
+		
+				(*json)[i] = ' ';
+		
+			}
+			
+		}
+	
+	fclose(jsonFile);
+	return 1;
+	}
+	
+	else { printf("Error : Could not open the file"); return 0;}
+}
+
