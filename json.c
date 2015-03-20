a79c4d253c5609402706c68d31a058fe8ee401f9
Kaliiixx <modepadu95@riseup.net>
2015-03-20 23:22:26 +0100

    functions splitted in different files

---
+++ json.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int extractJson(char file[], char **json)
+{
+	/* The aim of this fonction is to extract the Json file in 
+	 * a characters string in order to read its tokens
+	 */
+	int fileLength = 0, i = 0, c = 0, quote = 1;
+	FILE* jsonFile = NULL;
+
+	jsonFile = fopen(file, "r");
+	if ( jsonFile != NULL )
+	{		
+		for (fileLength=0; (c = fgetc(jsonFile)) != EOF; )
+		{
+			//don’t count only letters, numbers, and punctuation exept in the quotes
+			
+			if ( c == '"')
+			{
+				quote ++ ;
+
+			}
+
+			
+			if ( isalnum(c) || ispunct(c) ||  quote%2 == 0)
+			{
+				fileLength++ ;
+			}
+		}
+		
+		quote = 1 ;
+
+		//get amount of characters to allocate a right size string
+		*json = malloc(fileLength * sizeof(char));
+		
+		rewind(jsonFile);
+		
+		
+		for(i=0; i< fileLength; i++)
+		{
+			//write only readable characters		
+			c = fgetc(jsonFile);
+
+			if ( c == '"')
+			{
+				quote++ ;			
+			}
+		
+			if(isalnum(c) || ispunct(c) || quote%2 == 0)
+	
+			{
+		
+				(*json)[i] = (char)c;
+			}
+			
+			else
+			{
+				i--;						
+			}
+		
+		
+			
+		}
+	
+	fclose(jsonFile);
+	return 1;
+	}
+	
+	else { printf("Error : Could not open the file"); return 0;}
+}
+
