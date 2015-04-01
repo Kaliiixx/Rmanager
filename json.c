a9f145e4b6c449a7f39b85e809ea613bdc27c594
Kaliiixx <modepadu95@riseup.net>
2015-04-01 10:24:40 +0200

    implementing addBook()

--- json.c
+++ json.c
@@ -95,71 +95,91 @@ int addBook()
 	{
 		printf("\nTitle : ");
 		scanf("%s",&book.common.title);
-		
+		scanf("%c", NULL);
 		printf("\nCondition : ");
 		scanf("%c",&book.common.condition);
+		scanf("%c", NULL);
 
 		printf("\nLicence : ");
 		scanf("%s",&book.common.licence);
-		
+				scanf("%c", NULL);
+
 		printf("\nPlace : ");
 		scanf("%s",&book.common.place);
 		
 		printf("\nFormat : ");
 		scanf("%c",&book.common.format);
-		
+				scanf("%c", NULL);
+
 		printf("\nTitle : ");
 		scanf("%s",&book.common.title);
 		
 		printf("\nProcurement date : ");
 		scanf("%s",&book.common.procurement_date);
-		
+				scanf("%c", NULL);
+
 		printf("\nRelease date : ");
 		scanf("%s",&book.common.release_date);
+		scanf("%c", NULL);
 
 		printf("\nLanguage: ");
 		scanf("%s",&book.common.language);
+		scanf("%c", NULL);
 
 		printf("\nProcurement date : ");
 		scanf("%s",&book.common.procurement_date);
+		scanf("%c", NULL);
 
 		printf("\nnumber : ");
 		scanf("%d",&book.common.number);
+		scanf("%c", NULL);
 
 		printf("\nPublisher : ");
 		scanf("%s",&book.publisher);
-		
+				scanf("%c", NULL);
+
 		printf("\nSerie : ");
 		scanf("%s",&book.serie);
-		
+				scanf("%c", NULL);
+
 		printf("\nGenre : ");
 		scanf("%s",&book.genre);
+		scanf("%c", NULL);
 
 		printf("\nNumber of translators : ");
 		scanf("%d",&book.serie);
+		scanf("%c", NULL);
 
 		printf("\nTranslators : ");
 		scanf("%s",&book.serie);
-		
+				scanf("%c", NULL);
+
 		printf("\nVersion : ");
 		scanf("%s",&book.version);
+		scanf("%c", NULL);
 
 		printf("\nNumber of authors");  
 		scanf("%d",&book.nbAuthor);
+		scanf("%c", NULL);
 
 		printf("\nAuthors : ");
 		scanf("%s",&book.author);
+		scanf("%c", NULL);
 
 		printf("\nISBN : ");
 		scanf("%s",&book.isbn);
+		scanf("%c", NULL);
 
 		printf("\nPages : ");
 		scanf("%d",&book.page);
+		scanf("%c", NULL);
 
-		json_emit(buf, sizeof(buf),    }")
-
+		fprintf(jsonFile,"{type: \"BOOK\",{\"publisher\": \"%s\",\"series\": \"%s\",\"pages\": \"%d\",\"authors\":[\"%s\"],\"genre\": \"%s\",\"translator\":\"%s\",\"version\": \"%s\",\"isbn\": \"%s\",},\"common\" :{\"title\": \"%s\",\"condition\" : \"%c\",\"licence\": \"%s\",\"number\": \"%d\",\"procurement_date\":\"%s\",\"place\": \"%s\",\"release_date\": \"%s\",\"format\": \"%c\",\"language\": \"%s\",\"decription\":%s}}",book.publisher, book.serie, book.page, book.author, book.genre,book.translator, book.version, book.isbn, book.common.title, book.common.condition,book.common.licence, book.common.number, book.common.procurement_date, book.common.place,book.common.release_date, book.common.format, book.common.language, book.common.description);
+			
 		fclose(jsonFile);
 	}
 
 	return 0;
 }
+
+
