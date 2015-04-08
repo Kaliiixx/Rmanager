e336a69da331826751b9cc8fb658ff32170aad78
Kaliiixx <modepadu95@riseup.net>
2015-04-08 19:51:03 +0200

    Forgot these files

---
+++ json.h
@@ -0,0 +1,6 @@
+#ifndef JSON_H
+#define JSON_H
+
+int extractJson(char file[], char **json);
+int addBook();
+#endif
---
+++ macros.h
@@ -0,0 +1,20 @@
+#ifndef MACROS_H
+#define MACROS_H
+
+#define CLI
+
+
+#ifdef CLI
+
+#define READ(size,type,variable) readcli(size,type,variable);
+
+
+#endif
+
+#ifdef GUI
+
+
+
+#endif
+
+#endif
